86a3d990c09752aa6318a58e936071cc7571c08c
copygirl <copygirl@mcft.net>
2019-07-15 19:00:45 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,121 @@
+use amethyst::{
+    core::transform::TransformBundle,
+    ecs::prelude::{ReadExpect, Resources, SystemData},
+    prelude::*,
+    renderer::{
+        pass::DrawShadedDesc,
+        rendy::{
+            factory::Factory,
+            graph::{
+                render::{RenderGroupDesc, SubpassBuilder},
+                GraphBuilder,
+            },
+            hal::{format::Format, image},
+        },
+        types::DefaultBackend,
+        GraphCreator, RenderingSystem,
+    },
+    utils::application_root_dir,
+    window::{ScreenDimensions, Window, WindowBundle},
+};
+
+struct MyState;
+
+impl SimpleState for MyState {
+    fn on_start(&mut self, _data: StateData<'_, GameData<'_, '_>>) {}
+}
+
+const BACKGROUND_COLOR: [f32; 4] = [0.2, 0.4, 0.1, 1.0];
+
+fn main() -> amethyst::Result<()> {
+    amethyst::start_logger(Default::default());
+
+    let app_root = application_root_dir()?;
+    let assets_dir = app_root.join("assets");
+    let config_dir = app_root.join("config");
+    let display_config_path = config_dir.join("display.ron");
+
+    let game_data = GameDataBuilder::default()
+        .with_bundle(WindowBundle::from_config_path(display_config_path))?
+        .with_bundle(TransformBundle::new())?
+        .with_thread_local(RenderingSystem::<DefaultBackend, _>::new(
+            RenderingGraph::default(),
+        ));
+
+    let mut game = Application::new(assets_dir, MyState, game_data)?;
+    game.run();
+
+    Ok(())
+}
+
+#[derive(Default)]
+struct RenderingGraph {
+    dimensions: Option<ScreenDimensions>,
+    surface_format: Option<Format>,
+    dirty: bool,
+}
+
+impl GraphCreator<DefaultBackend> for RenderingGraph {
+    fn rebuild(&mut self, res: &Resources) -> bool {
+        // Rebuild when dimensions change, but wait until at least two frames have the same.
+        let new_dimensions = res.try_fetch::<ScreenDimensions>();
+        use std::ops::Deref;
+        if self.dimensions.as_ref() != new_dimensions.as_ref().map(|d| d.deref()) {
+            self.dirty = true;
+            self.dimensions = new_dimensions.map(|d| d.clone());
+            return false;
+        }
+
+        self.dirty
+    }
+
+    fn builder(
+        &mut self,
+        factory: &mut Factory<DefaultBackend>,
+        res: &Resources,
+    ) -> GraphBuilder<DefaultBackend, Resources> {
+        use amethyst::renderer::rendy::{
+            graph::present::PresentNode,
+            hal::command::{ClearDepthStencil, ClearValue},
+        };
+
+        self.dirty = false;
+        let window = <ReadExpect<'_, Window>>::fetch(res);
+        let surface = factory.create_surface(&window);
+        // cache surface format to speed things up
+        let surface_format = *self
+            .surface_format
+            .get_or_insert_with(|| factory.get_surface_format(&surface));
+        let dimensions = self.dimensions.as_ref().unwrap();
+        let window_kind =
+            image::Kind::D2(dimensions.width() as u32, dimensions.height() as u32, 1, 1);
+
+        let mut graph_builder = GraphBuilder::new();
+        let color = graph_builder.create_image(
+            window_kind,
+            1,
+            surface_format,
+            Some(ClearValue::Color(BACKGROUND_COLOR.into())),
+        );
+
+        let depth = graph_builder.create_image(
+            window_kind,
+            1,
+            Format::D32Sfloat,
+            Some(ClearValue::DepthStencil(ClearDepthStencil(1.0, 0))),
+        );
+
+        let opaque = graph_builder.add_node(
+            SubpassBuilder::new()
+                .with_group(DrawShadedDesc::new().builder())
+                .with_color(color)
+                .with_depth_stencil(depth)
+                .into_pass(),
+        );
+
+        let _present = graph_builder
+            .add_node(PresentNode::builder(factory, surface, color).with_dependency(opaque));
+
+        graph_builder
+    }
+}
