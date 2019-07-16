1d5af73f2b7308dc34fe6ea44995fd78c1c330ff
copygirl <copygirl@mcft.net>
2019-07-16 13:48:03 +0200

    Zoom camera depending on window dimensions

--- src/camera.rs
+++ src/camera.rs
@@ -18,6 +18,7 @@ impl<'s> System<'s> for CameraSystem {
         WriteStorage<'s, Transform>,
     );
 
+    // TODO: Only update Camera component when window dimensions change?
     fn run(&mut self, (entities, dimensions, mut cameras, mut transforms): Self::SystemData) {
         let entity = self.camera.get_or_insert_with(|| {
             let mut transform = Transform::default();
@@ -26,6 +27,10 @@ impl<'s> System<'s> for CameraSystem {
                 .with(transform, &mut transforms)
                 .build()
         });
-        cameras.insert(*entity, Camera::standard_2d(dimensions.width(), dimensions.height())).unwrap();
+
+        let zoom = (dimensions.width().min(dimensions.height()) / 200.0).floor().max(1.0);
+        let width = dimensions.width() / zoom;
+        let height = dimensions.height() / zoom;
+        cameras.insert(*entity, Camera::standard_2d(width, height)).unwrap();
     }
 }
