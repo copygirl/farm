e5656d76ede0e2e54aadc39fd6b5013b1b66ae84
copygirl <copygirl@mcft.net>
2019-07-26 00:30:27 +0200

    Add `render` module
    - Contains all rendering related tasks
    - Taken from official 2D starter template
      https://github.com/amethyst/amethyst-starter-2d
    - Supports transparency and sorting

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,22 @@
 use amethyst::{
     assets::Processor,
     core::transform::TransformBundle,
-    ecs::prelude::{ReadExpect, Resources, SystemData},
     input::{InputBundle, StringBindings},
     prelude::*,
     renderer::{
-        pass::DrawFlat2DDesc,
-        rendy::{
-            factory::Factory,
-            graph::{
-                render::{RenderGroupDesc, SubpassBuilder},
-                GraphBuilder,
-            },
-            hal::{format::Format, image},
-        },
+        sprite_visibility::SpriteVisibilitySortingSystem,
         types::DefaultBackend,
-        GraphCreator, RenderingSystem, SpriteSheet,
+        RenderingSystem, SpriteSheet
     },
     utils::application_root_dir,
-    window::{ScreenDimensions, Window, WindowBundle},
+    window::WindowBundle,
 };
 
 mod camera;
 mod controls;
 mod farm;
 mod movement;
-
-use crate::camera::CameraSystem;
-use crate::controls::ControlsSystem;
-use crate::farm::MainState;
-use crate::movement::MovementSystem;
-
-const BACKGROUND_COLOR: [f32; 4] = [0.2, 0.4, 0.1, 1.0];
+mod render;
 
 fn main() -> amethyst::Result<()> {
     amethyst::start_logger(Default::default());
@@ -42,91 +27,25 @@ fn main() -> amethyst::Result<()> {
     let display_config_path = config_dir.join("display.ron");
     let bindings_config_path = config_dir.join("bindings.ron");
 
+    let render_graph = render::RenderGraph::default();
+    let render_system = RenderingSystem::<DefaultBackend, _>::new(render_graph);
+
     let game_data = GameDataBuilder::default()
         .with_bundle(WindowBundle::from_config_path(display_config_path))?
         .with_bundle(InputBundle::<StringBindings>::new().with_bindings_from_file(bindings_config_path)?)?
         .with_bundle(TransformBundle::new())?
 
+        .with(SpriteVisibilitySortingSystem::new(), "sprite_visibility_system", &["transform_system"])
         .with(Processor::<SpriteSheet>::new(), "sprite_sheet_processor", &[])
 
-        .with(CameraSystem::default(), "camera_system", &[])
-        .with(ControlsSystem::default(), "controls_system", &[])
-        .with(MovementSystem::default(), "movement_system", &[])
+        .with(camera::CameraSystem::default(), "camera_system", &[])
+        .with(controls::ControlsSystem::default(), "controls_system", &[])
+        .with(movement::MovementSystem::default(), "movement_system", &[])
 
-        .with_thread_local(RenderingSystem::<DefaultBackend, _>::new(RenderingGraph::default()));
+        .with_thread_local(render_system);
 
-    let mut game = Application::new(assets_dir, MainState, game_data)?;
+    let mut game = Application::new(assets_dir, farm::MainState, game_data)?;
     game.run();
 
     Ok(())
-}
-
-#[derive(Default)]
-struct RenderingGraph {
-    dimensions: Option<ScreenDimensions>,
-    surface_format: Option<Format>,
-    dirty: bool,
-}
-
-impl GraphCreator<DefaultBackend> for RenderingGraph {
-    fn rebuild(&mut self, res: &Resources) -> bool {
-        // Rebuild when dimensions change, but wait until at least two frames have the same.
-        let new_dimensions = res.try_fetch::<ScreenDimensions>();
-        use std::ops::Deref;
-        if self.dimensions.as_ref() != new_dimensions.as_ref().map(|d| d.deref()) {
-            self.dirty = true;
-            self.dimensions = new_dimensions.map(|d| d.clone());
-            return false;
-        }
-
-        self.dirty
-    }
-
-    fn builder(
-        &mut self,
-        factory: &mut Factory<DefaultBackend>,
-        res: &Resources,
-    ) -> GraphBuilder<DefaultBackend, Resources> {
-        use amethyst::renderer::rendy::{
-            graph::present::PresentNode,
-            hal::command::{ClearDepthStencil, ClearValue},
-        };
-
-        self.dirty = false;
-        let window = <ReadExpect<'_, Window>>::fetch(res);
-        let dimensions = self.dimensions.as_ref().unwrap();
-        let window_kind = image::Kind::D2(dimensions.width() as u32, dimensions.height() as u32, 1, 1);
-
-        let surface = factory.create_surface(&window);
-        let surface_format = *self.surface_format
-            .get_or_insert_with(|| factory.get_surface_format(&surface));
-
-        let mut graph_builder = GraphBuilder::new();
-        let color = graph_builder.create_image(
-            window_kind,
-            1,
-            surface_format,
-            Some(ClearValue::Color(BACKGROUND_COLOR.into())),
-        );
-
-        let depth = graph_builder.create_image(
-            window_kind,
-            1,
-            Format::D32Sfloat,
-            Some(ClearValue::DepthStencil(ClearDepthStencil(1.0, 0))),
-        );
-
-        let opaque = graph_builder.add_node(
-            SubpassBuilder::new()
-                .with_group(DrawFlat2DDesc::new().builder())
-                .with_color(color)
-                .with_depth_stencil(depth)
-                .into_pass(),
-        );
-
-        let _present = graph_builder
-            .add_node(PresentNode::builder(factory, surface, color).with_dependency(opaque));
-
-        graph_builder
-    }
 }
